dfbbec31c926d6446c5abc673d5f1febbe59525b
+ <+>
2023-08-21 14:06:39 +0300

    used enumerate

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -5,15 +5,8 @@ def batch_badge_creator(names):
     return [badge_maker(name) for name in names]  
 
 def assign_rooms(names):
-    room_assignments = []
-    room_number = 1
+   return [f"Hello, {name}! You'll be assigned to room {index+1}!" for index, name in enumerate(names)]
 
-    for name in names:
-        room_assignment = f"Hello, {name}! You'll be assigned to room {room_number}!"
-        room_assignments.append(room_assignment)
-        room_number += 1
-
-    return room_assignments
 
 
 def printer(names):
